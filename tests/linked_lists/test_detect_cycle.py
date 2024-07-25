aadf70ccc442dce45ef9a4948092637aabe9ab80
aitoolkit <med.taha.elahmar@gmail.com>
2024-07-26 00:00:09 +0200

    solve detect cycle challenge

--- tests/linked_lists/test_detect_cycle.py
+++ tests/linked_lists/test_detect_cycle.py
@@ -1,4 +1,14 @@
-from topics.linked_lists.detect_cycle import has_cycle
+from topics.linked_lists.detect_cycle import has_cycle, Node
+
 
 def test_has_cycle():
-    assert True
+    node1 = Node(1)  # type: ignore
+    node2 = Node(2)  # type: ignore
+    node3 = Node(3)  # type: ignore
+    node4 = Node(4)  # type: ignore
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2
+    
+    assert has_cycle(node1)
--- topics/linked_lists/detect_cycle.py
+++ topics/linked_lists/detect_cycle.py
@@ -1,2 +1,16 @@
-def has_cycle():
-    pass
+class Node:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+def has_cycle(head: Node) -> int:
+    nodes = set()
+    nodes.add(head)
+    current = head
+    while (current.next):
+        if current.next in nodes:
+            return 1
+        current = current.next
+        nodes.add(current)
+    return 0
