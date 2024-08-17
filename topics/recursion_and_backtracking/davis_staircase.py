364c7173c0ea0a51c3fc8b8028b8fcbbb9fc0318
aitoolkit <med.taha.elahmar@gmail.com>
2024-08-17 23:52:36 +0200

    update Davis staircase step perm function

--- topics/recursion_and_backtracking/davis_staircase.py
+++ topics/recursion_and_backtracking/davis_staircase.py
@@ -16,6 +16,7 @@ def stepPerms(n):
     dp[0], dp[1], dp[2], dp[3] = 1, 1, 2, 4
     
     for i in range(4, n + 1):
+        
         dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD
     
     return dp[n]
