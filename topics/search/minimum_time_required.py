92d9fff7f9e46aee0018db9a9d0fc5f57756eb22
aitoolkit <med.taha.elahmar@gmail.com>
2024-08-18 23:16:23 +0200

    solve min time required

--- topics/search/minimum_time_required.py
+++ topics/search/minimum_time_required.py
@@ -1,15 +1,32 @@
+# Sol 1 : Time consuming
+# def minTime(machines, goal):
+#     print(machines)
+#     print(goal)
+    
+#     days_min = int(goal / (sum([1/m for m in machines])))
+    
+#     days = days_min
+#     count = sum([days//machine for machine in machines])
+    
+#     while count < goal:
+#         days += 1
+#         count = sum([days//machine for machine in machines])
+    
+#     return days
 
+# Sol 2 : Optimal
 def minTime(machines, goal):
-    print(machines)
-    print(goal)
     
-    days_min = int(goal / (sum([1/m for m in machines])))
+    left = 1
+    right = max(machines) * goal
     
-    days = days_min
-    count = sum([days//machine for machine in machines])
-    
-    while count < goal:
-        days += 1
-        count = sum([days//machine for machine in machines])
-    
-    return days
+    while left < right:
+        mid = (left + right) // 2
+        total_items = sum(mid // m for m in machines)
+        
+        if total_items >= goal:
+            right = mid
+        else:
+            left = mid + 1
+        
+    return left
